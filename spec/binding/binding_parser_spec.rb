22af03be13d42dcc5e12bba144f0f9377032d995
Christoph Brand <ch.brand@scolterius.net>
2016-03-07 01:27:54 +0100

    binding: added parser for query binding entries and added tests to verify correct behavior.

---
+++ spec/binding/binding_parser_spec.rb
@@ -0,0 +1,46 @@
+require 'rspec'
+require 'parslet/rig/rspec'
+require_relative '../../lib/binding/binding_parser'
+require 'parslet/convenience'
+
+
+
+describe Filterparams::BindingParser do
+
+  let(:parser) { described_class.new }
+
+  it 'should be able to parse a simple parameter' do
+    parsed = parser.parse 'parameter'
+
+    expect(parsed).to eq({parameter: 'parameter'})
+  end
+
+  it 'should be able to parse an and construct' do
+    expect(parser).to parse('parameter & otherParameter')
+  end
+
+  it 'should be able to parse an or construct' do
+    expect(parser).to parse('parameter | otherParameter')
+  end
+
+  it 'should be able to parse a negation' do
+    expect(parser).to parse('!parameter')
+  end
+
+  it 'should be able to parse a bracket' do
+    expect(parser).to parse('( a & b )')
+  end
+
+  it 'should be able to parse a complex construct' do
+    expect(parser).to parse('(a|b) & c | d')
+  end
+
+  it 'should be able to parse a bracket at the end of a complex construct' do
+    expect(parser).to parse('a|b& ( c | d)')
+  end
+
+  it 'should be able to parse nested statements with brackets' do
+    expect(parser).to parse('(a | b)&!( b)|c& e')
+  end
+
+end
