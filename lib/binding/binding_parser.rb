ce7027971c4deed14cbff13f0ff347aaab19adc0
Christoph Brand <ch.brand@scolterius.net>
2016-03-07 01:27:54 +0100

    binding: added parser for query binding entries and added tests to verify correct behavior.

---
+++ lib/binding/binding_parser.rb
@@ -0,0 +1,35 @@
+require 'parslet'
+
+
+module Filterparams
+  class BindingParser < Parslet::Parser
+    rule(:space) { match('\s').repeat(1) }
+    rule(:space?) { space.maybe }
+
+    rule(:lparen) { str('(') >> space? }
+    rule(:rparen) { str(')') >> space? }
+
+    rule(:and_operator) { str('&') }
+    rule(:or_operator) { str('|') }
+
+    rule(:parameter) { match('[\w\-\_]').repeat(1).as(:parameter) }
+
+    rule(:bracket) { lparen >> space? >> clause.as(:clause) >> space? >> rparen }
+
+    rule(:and_clause_element) { parameter | bracket }
+    rule(:and_clause) {
+      and_clause_element.as(:left) >> space? >> and_operator.as(:operator) >> space? >> (and_clause | and_clause_element).as(:right)
+    }
+    rule(:or_clause_element) { and_clause | parameter | bracket }
+    rule(:or_clause) {
+      or_clause_element.as(:left) >> space? >> or_operator.as(:operator) >> space? >> clause.as(:right)
+    }
+    rule(:not_operation) { str('!').as(:operator) >> space? >> clause.as(:clause) }
+
+    rule(:clause) { or_clause | and_clause | parameter | bracket | not_operation }
+
+    rule(:binding) { space? >> clause >> space? }
+
+    root(:binding)
+  end
+end
